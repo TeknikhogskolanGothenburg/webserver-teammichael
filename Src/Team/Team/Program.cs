569054ae67ef56056844cd81b1c347b6c19f294e
jaekthesnaek <32323708+jaekthesnaek@users.noreply.github.com>
2018-01-11 11:34:39 +0100

    återställt till gamla versionen

--- Src/Team/Team/Program.cs
+++ Src/Team/Team/Program.cs
@@ -4,24 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
-using System.IO;
 
 namespace WebServer
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] prefixes)
         {
-            WebServ ws = new WebServ(SendResponse, "http://localhost:8080/");
-            ws.Run();
-            Console.WriteLine("A simple webserver. Press a key to quit.");
-            Console.ReadKey();
-            ws.Stop();
-        }
 
-        public static string SendResponse(HttpListenerRequest request)
-        {
-            return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now);
+            // This example requires the System and System.Net namespaces.
+
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                return;
+            }
+            // URI prefixes are required,
+            // for example "http://contoso.com:8080/index/".
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+
+            // Create a listener.
+            HttpListener listener = new HttpListener();
+            // Add the prefixes.
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+            Console.WriteLine("Listening...");
+            // Note: The GetContext method blocks while waiting for a request. 
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            // Obtain a response object.
+            HttpListenerResponse response = context.Response;
+            // Construct a response.
+            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+            listener.Stop();
+
         }
     }
 }
