f168d785e6825539acef965c62d1d8eedc05c0a5
Michae Alamin <michaelalamin@gmail.com>
2018-01-14 20:34:43 +0100

    Återställ Gammal Versionen

--- Src/Team/Team/Program.cs
+++ Src/Team/Team/Program.cs
@@ -3,34 +3,118 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Net.Sockets;
-using System.Threading;
+using System.Net;
 using System.IO;
-using System.Web;
-using System.Diagnostics;
-using webserver;
 
 namespace WebServer
 {
     class Program
-
-
     {
-
-        static void Main(string[] args)
+        static void Main()
 
         {
+            GetFiles();
+        }
+        private static void server(string[] prifixes)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                return;
+            }
+            // URI prifixes are required,
+            // for example "http://contoso.com:8080/index/".
+            if (prifixes == null || prifixes.Length == 0)
+                throw new ArgumentException("prifixes");
+            // Create a listener.
+            HttpListener listener = new HttpListener();
+            // Add the prifixes.
+            foreach (string s in prifixes)
+            {
+                listener.Prefixes.Add(s);
+                //   Console.WriteLine("Listening...");
 
-            Console.WriteLine("Starting seever om the port 8080 ");
-            HTTPServer server = new HTTPServer(8080);
+            }
 
-            server.start();
+            while (true)
+
+            {
+
+                listener.Start();
+                //  Console.WriteLine("Listening..." + );
+
+                // Note: The GetContext method blocks while waiting for a request. 
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                // Construct a response.
 
 
+                //string responseString = File.ReadAllText(");
+
+                //////      string responseString"<HTML><BODY> Hello world!</BODY></HTML>";
+
+
+                byte[] buffer = File.ReadAllBytes(Directory.GetCurrentDirectory() + "/" + "Content" + request.RawUrl);
+
+                //    //System.Text.Encoding.UTF8.GetBytes(responseString);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                //You must close the output stream.
+                output.Close();
+                listener.Stop();
+
+            }
         }
 
+
+  
+
+        private static void GetFiles()
+
+        {
+            string[] fileEntries = Directory.GetFiles("Content", "*", SearchOption.AllDirectories);
+
+            string[] url = new string[fileEntries.Length];
+
+            //foreach (string x in url)
+
+            //{
+
+            //    Console.WriteLine(x);
+            //}
+
+
+
+
+            for (int i = 0; i < fileEntries.Length; i++)
+
+            {
+
+                string st = "Cookies  ";
+                int x = 0;
+                x++;
+
+                url[i] = "http://localhost:8080/" + fileEntries[i].Substring("Content".Length + 1).Replace('\\', '/') + "/";
+
+                Console.WriteLine(x + i); // cookies 
+                Console.WriteLine(url[i]);
+            }
+
+
+            Console.WriteLine("--------------------------------------------------------  ");
+            Console.WriteLine("Antal sidor  " + fileEntries.Length);
+            /* Cookies är en data som en webbserver 
+            kan be att få spara i webbplatsbesökarens dator. Men i det här fallet blir det 
+            våra cookies är hur mycket sidor vi har om jag har förstår den correct.  
+    */
+            server(url);
+
+        }
 
     }
 
 }
-
